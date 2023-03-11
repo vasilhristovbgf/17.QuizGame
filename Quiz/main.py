741b43512ab68687be691d614678f5b93aff3ac0
VasilHristov <vasil.hristovbgf@gmail.com>
2023-03-11 21:36:24 +0100

    v03

--- Quiz/main.py
+++ Quiz/main.py
@@ -1,6 +1,10 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+import os
+import time
+
+
 question_bank = []
 n_quest = QuizBrain(question_bank)
 for question in question_data:
@@ -9,5 +13,15 @@ for question in question_data:
     new_question = Question(q_text, q_answer)
     question_bank.append(new_question)
 while n_quest.still_has_questions():
+
+    if n_quest.question_number == 0:
+        input('Press Enter to start..')
+    elif n_quest.question_number > 0:
+        input('Press Enter to continue..')
+    os.system('cls')
+
     pl_ans = n_quest.next_question()
     n_quest.is_correct(pl_ans, n_quest.question_list, n_quest.question_number)
+    time.sleep(0.25)
+
+input('Press Enter to quit..')
--- Quiz/quiz_brain.py
+++ Quiz/quiz_brain.py
@@ -2,26 +2,30 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     def next_question(self):
 
         curr_qt = self.question_list[self.question_number].text
 
-        player_answer = input(f"Q{self.question_number}. {curr_qt} ")
+        player_answer = input(f"Q{self.question_number + 1}. {curr_qt} ")
         return player_answer
 
     def still_has_questions(self):
 
         if self.question_number < len(self.question_list):
+            print(f'Current Score: {self.score}/{self.question_number}')
             return True
         elif self.question_number == len(self.question_list):
+            print(f'Final Score: {self.score}/{self.question_number}')
             return False
 
     def is_correct(self, pl_answ, q_list, q_num):
         if pl_answ == q_list[q_num].answer:
-            print("That's Correct!")
+            self.score += 1
+            print('That`s Correct!')
             self.question_number += 1
-            return True
         else:
-            print("Wrong answer!")
-            return False
+            print('Wrong answer!')
+            self.question_number += 1
+        return True
